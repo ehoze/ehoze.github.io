ed5e3a7f0810cf6e0b1bb1bbbd4c0774dec1e004
ehoze <eryk_kucharski@tlen.pl>
2021-09-09 21:35:19 +0200

    Pierwsza aktualizacja

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+var menuButton = document.querySelector('.navmenu');
+var menuBg = document.querySelector('.modal-bg');
+var menuClose = document.querySelector('.modal-close');
+
+menuButton.addEventListener('click', function() {
+    menuBg.classList.add('bg-active');
+    document.querySelector('.fa-caret-square-down').style.transform = "rotate(180deg)";
+});
+
+menuClose.addEventListener('click', function() {
+    menuBg.classList.remove('bg-active');
+    document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
+});
