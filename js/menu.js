b36ed3151d59ac493864e68f70f37e013daea23d
ehoze <eryk_kucharski@tlen.pl>
2021-09-14 19:06:26 +0200

    added portfolio site, some css, update includes changes in code, you can now leave menu by clicking somewhere on the screen, made contact site responsible for phone

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
 var menuButton = document.querySelector('.navmenu');
 var menuBg = document.querySelector('.modal-bg');
 var menuClose = document.querySelector('.modal-close');
+var sitebody = document.body;
 
 menuButton.addEventListener('click', function() {
     menuBg.classList.add('bg-active');
+    sitebody.classList.add('blur');
     document.querySelector('.fa-caret-square-down').style.transform = "rotate(180deg)";
 });
 
 
+// menuButton.addEventListener(''), function() {
+//     menuBg.classList.remove('bg-active');
+//     sitebody.classList.remove('blur');
+//     document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
+// });
+
 menuClose.addEventListener('click', function() {
     menuBg.classList.remove('bg-active');
+    sitebody.classList.remove('blur');
+    document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
+});
+
+menuBg.addEventListener('click', function(){
+    menuBg.classList.remove('bg-active');
+    sitebody.classList.remove('blur');
     document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
 });
