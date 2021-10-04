afb218f2fe82e01ead2a8afe7ad5ba2977f65cb2
ehoze <eryk_kucharski@tlen.pl>
2021-10-04 21:35:29 +0200

    Full site redesign

--- examples/Shop_gallery/script/script.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById("sale").innerHTML += '<h3>Wyprzedaż!</h3>';
-var allElements = document.getElementsByClassName('sale');
-Array.prototype.forEach.call(allElements, function(ele) {
-    ele.innerHTML += '<h3 class="absolute" style="top: 10px;">Wyprzedaż!</h3>';
-});
--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,8 @@
-var menuButton = document.querySelector('.navmenu');
-var menuBg = document.querySelector('.modal-bg');
-var menuClose = document.querySelector('.modal-close');
-var sitebody = document.body;
+const hamburger = document.querySelector('.header .nav_bar .nav_list .hamburger');
+const mobile = document.querySelector('.header .nav_bar .nav_list ul');
+const header = document.querySelector('.header.container');
 
-menuButton.addEventListener('click', function() {
-    menuBg.classList.add('bg-active');
-    sitebody.classList.add('blur');
-    document.querySelector('.fa-caret-square-down').style.transform = "rotate(180deg)";
-});
-
-
-// menuButton.addEventListener(''), function() {
-//     menuBg.classList.remove('bg-active');
-//     sitebody.classList.remove('blur');
-//     document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
-// });
-
-menuClose.addEventListener('click', function() {
-    menuBg.classList.remove('bg-active');
-    sitebody.classList.remove('blur');
-    document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
-});
-
-menuBg.addEventListener('click', function(){
-    menuBg.classList.remove('bg-active');
-    sitebody.classList.remove('blur');
-    document.querySelector('.fa-caret-square-down').style.transform = "rotate(0deg)";
+hamburger.addEventListener('click',()=>
+{
+    hamburger.classList.toggle('active');
 });
