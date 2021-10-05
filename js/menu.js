d696faa9880d2fb74f0360d32cea793f0de7c5a2
ehoze <eryk_kucharski@tlen.pl>
2021-10-05 20:42:03 +0200

    Hotfix -- First data placed

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,29 @@
 const hamburger = document.querySelector('.header .nav_bar .nav_list .hamburger');
 const mobile = document.querySelector('.header .nav_bar .nav_list ul');
+const option = document.querySelectorAll('.header .nav_bar .nav_list ul li a');
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click',()=>
 {
     hamburger.classList.toggle('active');
+    mobile.classList.toggle('active');
+});
+
+document.addEventListener('scroll', ()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250)
+    {
+        header.style.backgroundColor = "#000";
+    }
+    else
+    {
+        header.style.backgroundColor = "transparent";
+    }
+})
+
+option.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile.classList.toggle('active');
+    });
 });
